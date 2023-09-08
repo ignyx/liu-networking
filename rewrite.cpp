e808991489261addb5bfb85447a6dd9f4d701fda
J-Bourgeais <143534065+J-Bourgeais@users.noreply.github.com>
2023-09-08 11:59:47 +0200

    Update rewrite.cpp
    Add the check for another word.
    
    Don't replace the word if image.

--- rewrite.cpp
+++ rewrite.cpp
@@ -7,51 +7,77 @@ using namespace std;
 //See how to take file and carry them segment by segment
 
 
-"""
-Pour l'instant : juste recherche et remplacement de mot
-"""
+//separer en fct : searchWord, replaceWord
+
+
+//les <img src="./smiley.jpg" alt="Smiley smiling!" width="200" height="200"> should be replace by our own images
 
 
 // Function to search for a word octet by octet
-bool searchWord(string& text, const string& word, const string& replaceWord) {
-    int textLength = text.length();
-    int wordLength = word.length();
 
-    for (int i = 0; i <= textLength - wordLength; i++) { //condition on i in order to not depasser le nbr de mot du texte
-        bool found = true;
-        for (int j = 0; j < wordLength; j++) {
-            if (text[i + j] != word[j]) {
-                found = false;
+
+//return a partie memoire ?
+
+void searchinFile(string& text, const string& word1, const string& replaceWord1, const string& word2, const string& replaceWord2) {
+    int textLength = text.length();
+    int maxLength = max(word1.length(), word2.length());
+    int word1length = word1.length();
+    int word2length = word2.length();
+    for (int i = 0; i <= textLength - maxLength; i++) { //condition on i in order to go above the number of words in the text
+        bool found1 = true;
+
+        for (int j = 0; j < word1length; j++) {
+            if (text[i + j] != word1[j]) {
+                found1 = false;
                 break;
                 //If we are not false but there are no letter left : create a memory (or in out) and check with the other segment
-                //--> reprendre la ou on en est meme avec un autre segment
+                //--> Continue at the same point but with the other segment
             }
         }
-        if (found) {
-            text.replace(i, wordLength, replaceWord); // see Notion for link : .replace (position, taille, mot qui remplace)
-            return true;
-            //Need to check if we can replace it (not stokholm in an image name for example) --> Look at the character around ?
-            //Need to change the content length : If Linköping because of the ö. How to access it ?
+        bool found2 = true;
+        for (int k = 0 ; k < word2length ; k++) {
+            if (text[i + k] != word2[k]) {
+                found2 = false;
+                break;
+                //If we are not false but there are no letter left : create a memory (or in out) and check with the other segment
+                //--> Continue at the same point but with the other segment
+            }
+        }
+        if (found1) {
+            //Replace a word : https://cplusplus.com/reference/string/string/replace/
+            if (text[i-1]=='/') {
+                cout << "A faire" << endl;
+                //Change Content-Length Header (our length)
+                //replace with our link to the photo of trolly
+            } else {
+                text.replace(i, word1length, replaceWord1);
+                found1=true;
+            }
+        }
+        if (found2) {
+            if (text[i-1]=='/'){
+                break; //Don't replace if the word is the name of an image
+            } else {
+                text.replace(i, word2length, replaceWord2); 
+                //Change Content-Length Header (+1)
+                found2=true;
+            }
+                
         }
     }
-
-    return false;
 }
 
 int main() {
     string Text;
-    string sWord {"Smiley"};
-    string replaceWord {"Trolly"};
+    string sWord1 {"Smiley"};
+    string replaceWord1 {"Trolly"};
+    string sWord2 {"Stockholm"};
+    string replaceWord2 {"Linköping"};
 
-    cout << "Enter the text : "; //Pour tester, on aura le segment
+    cout << "Enter the text : "; //To test. We will have the segment
     getline(cin, Text);
 
-
-    if (searchWord(Text, sWord, replaceWord)) { //Pour tester, on renverra au client
-        cout << "Word found in the text." << endl;
-    } else {
-        cout << "Word not found in the text." << endl;
-    }
+    searchinFile(Text, sWord1, replaceWord1, sWord2, replaceWord2);
     cout << Text << endl;
     return 0;
 }
